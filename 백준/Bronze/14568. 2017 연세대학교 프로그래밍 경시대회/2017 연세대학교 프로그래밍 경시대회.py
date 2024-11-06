0d726a35c524c4707c457b451be92ec15362a0dd
winsome99 <94812036+jekk7789@users.noreply.github.com>
2024-11-06 17:20:40 +0900

    [Bronze III] Title: 2017 연세대학교 프로그래밍 경시대회, Time: 136 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.py
@@ -0,0 +1,11 @@
+candy = int(input())
+result = 0
+for A in range(0, candy+1):
+    for B in range(0, candy+1):
+        for C in range(0, candy+1):
+            if(A+B+C == candy):
+                if(A >= B+2):
+                    if(A !=0 and B !=0 and C !=0):
+                        if(C % 2 == 0):
+                            result += 1
+print(result)
